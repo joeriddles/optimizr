0de98f0da7d6f205ecbc6120f27bd23e7e1d9249
Joseph Riddle <joeriddles10@gmail.com>
2021-04-07 13:15:31 -0700

    Fix __init__

--- optimizr/__init__.py
+++ optimizr/__init__.py
@@ -1,11 +1,5 @@
 from .GroupOptimizations3 import (
     binarize_groups,
-    calculate_exraneous_penalty,
+    binarize_target_group,
     find_optimal_groups
 )
-
-__all__ = [
-    'binarize_groups',
-    'calculate_exraneous_penalty',
-    'find_optimal_groups',
-]
