753f65e4218a5848a0496525f72c7ceeddcfe389
Joseph Riddle <joeriddles10@gmail.com>
2021-04-07 18:45:20 -0700

    fix(optimizr): Fix setup

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
 import setuptools
 
+import optimizr
 
-setuptools.setup()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+version = optimizr.__version__
+
+
+setuptools.setup(
+    name="optimizr",
+    version=version,
+    author="Joseph Riddle, Clayton Gravatt",
+    author_email="joeriddles10@gmail.com",
+    description="A small package to find similar collections.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joeriddles/optimizr",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
